0abba0cd845837b2e7edea5795e8bebbdc83b651
AliSaghaei <79021514+AliSaghaei@users.noreply.github.com>
2021-12-16 09:29:34 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// ِDeclare and assign a variable
+let ali = 20;
+
+// playing  with operators
+ali = 21;
+
+ali += 2;
+
+ali -= 5;
+
+ali++;
+ali++;
+
+ali--;
+
+// boolean
+let bool = true;
+
+let userName = "Amir";
+
+let longQuote = "alex let's go to the party. alex";
+
+//Skateboard quiz
+let quiz = "Skate Board";
+console.log(quiz.slice(5).replace("o", "e"));
+
+// Template literal
+let template = `Hello, ${userName}`;
+
+// Math and getting a random number from 0 to 10
+console.log(Math.floor(Math.random() * 10));
